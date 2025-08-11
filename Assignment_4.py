2ef7731b6818a0654ddca5491191319ae76f5c3a
virat-ie-9 <Golu61540@gmail.com>
2025-08-11 08:31:03 +0530

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,28 @@
+### TASK1: read a file and handle errors
+
+try:
+    file1 = open('sample.txt', 'r')
+    result = file1.read()
+    print(result)
+    file1.close()
+except:
+    FileNotFoundError
+    print('Error: The file \'sample.txt\' was not found.')
+
+### TASK2: Write and append Data to a file:
+
+n = input("Enter text to write to the file: ")
+file2 = open('output.txt', 'w')
+result = file2.write(n + '\n')                       # take inputs from n.., then after starts a new line.
+print("Data successfully written to output.txt.")
+
+m = input("Enter additional text to append: ")
+file2 = open('output.txt', 'a')
+result = file2.write(m)
+print("Data successfully appended.")
+
+print("Final content of output.txt: ")
+
+file2 = open('output.txt', 'r')
+result = file2.read()
+print(result)
